33cd5a91202469ec516bdc705e338c9751b337ca
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-28 11:46:46 -0500

    work on jQuery intro

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("", );
+console.log("jQuery!!!!");
+
+var vanillaStuff = document.getElementById("list-of-stuff");
+console.log("DOM element", vanillaStuff);
+vanillaStuff.classList.add("myList");
+
+var jQStuff = $("#list-of-stuff");
+console.log("JQ", jQStuff);
+
+jQStuff.addClass("myList");
+// jQStuff.append("<li>Hamburgers</li>");
+jQStuff.prepend("<li>Hamburgers</li>");
+
+
+
+
+
+
+
